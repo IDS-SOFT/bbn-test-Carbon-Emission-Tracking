a58c9b0b0c0622dc79e490818e0da07735ad2fa9
NikinikitaN <119474174+NikinikitaN@users.noreply.github.com>
2023-09-22 17:43:52 +0530

    Add files via upload

---
+++ test/index.ts
@@ -0,0 +1,6 @@
+import { expect } from "chai";
+import { ethers } from "hardhat";
+
+const { loadFixture } = require("@nomicfoundation/hardhat-network-helpers");
+
+//describe("CarbonEmissionTracking", function () {});
